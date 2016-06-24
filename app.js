8991a302794429b5aa5b1786e5061e0af1d75270
olds1423 <olds1423@gmail.com>
2016-06-23 21:10:15 -0700

    I have an idea

---
+++ app.js
@@ -0,0 +1,24 @@
+// Lets crush this!
+function catalogItem(name, path) {
+  this.name = null;
+  this.filepath = path;
+  this.numClicks = 0;
+  this.timeShown = 0;
+
+// I want a method that creates a random ? index
+//Do I want the event handler here?
+catalogItem.prototype.handleImgClick = function () {
+  for (var i = 0; i < numClicks; i++) {
+    // button event listener for image clicks?
+    // ranomize?
+  } else {
+    // create a button and insert it into the html
+    // handle review answer button click
+  }
+
+};
+}
+
+// event handler on click
+
+button.addEventListener("click", handleButtonClick);
