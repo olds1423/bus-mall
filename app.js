34259cc17e3f1a0d4561085899c99931246e720c
olds1423 <olds1423@gmail.com>
2016-06-25 10:40:43 -0700

    we add en empty array that will hold new instances and finished the constructor

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 // Lets crush this!
-function catalogItem(name, path) {
-  this.name = null;
+var totalNumClicks = 14;
+var actualClicks = 0;
+var button = document.getElementById("imgDisplay");
+var allItems = [];
+
+function CatalogItem(name, path) {
+  this.name = name;
   this.filepath = path;
   this.numClicks = 0;
   this.timeShown = 0;
-
+  allItems.push(this);
+ }
 // I want a method that creates a random ? index
 //Do I want the event handler here?
-catalogItem.prototype.handleImgClick = function () {
-  for (var i = 0; i < numClicks; i++) {
-    // button event listener for image clicks?
-    // ranomize?
-  } else {
-    // create a button and insert it into the html
-    // handle review answer button click
-  }
+  // CatalogItem.prototype.handleImgClick = function() {
+  //   for (var i = 0; i < totalNumClicks; i++) {
+  //     // button event listener for image clicks?
+  //     button.addEventListener("click", handleImgClick);
+  //     alert("Work");
+  //     // ranomize?
+  //     // create a button and insert it into the html
+  //     // handle review answer button click
+  //   }
+  //
+  // };
 
-};
-}
+var pen = new CatalogItem("pen", "img/pen.png");
+//Is this the correct way to path an image ^ ********
+div.appendChild(imgOne);
+//can I put the append child inside of the construtor ? *********
+
+handleImgClick.catalogItem();
 
 // event handler on click
 
-button.addEventListener("click", handleButtonClick);
+// button.addEventListener("click", handleButtonClick);
